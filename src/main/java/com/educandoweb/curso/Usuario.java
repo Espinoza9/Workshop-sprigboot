a68da28de38f4628bdfea11ab4173331656826cb
Espinoza9 <ge7384005@gmail.com>
2023-11-16 16:46:31 -0400

    recurso usuario

---
+++ src/main/java/com/educandoweb/curso/Usuario.java
@@ -0,0 +1,66 @@
+package com.educandoweb.curso;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Usuario implements Serializable {
+    private Long id;
+    private String  nome;
+    private  String email;
+    private String telefone;
+    private String senha;
+
+    public Usuario(Long id, String nome, String email, String telefone, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
